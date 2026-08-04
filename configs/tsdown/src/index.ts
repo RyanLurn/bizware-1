9ab37bae5c5b35f82a32a512f2dba90cd42b3957
Ryan Luong <lumintriet@gmail.com>
2026-08-04 09:43:10 +0700

    feat(tsdown-config): define 2 create config functions.

---
+++ configs/tsdown/src/index.ts
@@ -0,0 +1,39 @@
+import type { UserConfig } from "tsdown";
+
+import babel from "@rolldown/plugin-babel";
+import { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "tsdown";
+
+export function createLibConfig({
+  platform = "neutral",
+  exports = true,
+}: Pick<UserConfig, "platform" | "exports">): UserConfig {
+  return {
+    entry: [
+      "src/**/*.{ts,tsx}",
+      "!src/**/*.{test,try}.{ts,tsx}",
+      "!src/try.ts",
+    ],
+    dts: {
+      sourcemap: true,
+    },
+    unbundle: true,
+    platform,
+    exports,
+  };
+}
+
+export function createReactLibConfig({
+  platform = "neutral",
+  exports = true,
+}: Pick<UserConfig, "platform" | "exports">) {
+  const libConfig = createLibConfig({ platform, exports });
+  return defineConfig({
+    ...libConfig,
+    plugins: [
+      babel({
+        presets: [reactCompilerPreset()],
+      }),
+    ],
+  });
+}
