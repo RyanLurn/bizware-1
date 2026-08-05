14acf1f85990b07a487641faed84c3abe325b739
Ryan Luong <lumintriet@gmail.com>
2026-08-05 09:21:34 +0700

    feat(db-schema/relations): define auth relations.

---
+++ libs/db-schema/src/relations.ts
@@ -0,0 +1,37 @@
+import { defineRelations } from "drizzle-orm";
+
+import { accountTable, sessionTable, verificationTable } from "@/tables/auth";
+import { userTable } from "@/tables/user";
+
+export const relations = defineRelations(
+  {
+    userTable,
+    sessionTable,
+    accountTable,
+    verificationTable,
+  },
+  ({ many, one, userTable, sessionTable, accountTable }) => ({
+    userTable: {
+      sessions: many.sessionTable({
+        from: userTable.id,
+        to: sessionTable.userId,
+      }),
+      accounts: many.accountTable({
+        from: userTable.id,
+        to: accountTable.userId,
+      }),
+    },
+    sessionTable: {
+      user: one.userTable({
+        from: sessionTable.userId,
+        to: userTable.id,
+      }),
+    },
+    accountTable: {
+      user: one.userTable({
+        from: sessionTable.userId,
+        to: userTable.id,
+      }),
+    },
+  }),
+);
