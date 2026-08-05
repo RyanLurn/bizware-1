1d4f7add80f49f75ea1d18088f50539e1e704e5d
Ryan Luong <lumintriet@gmail.com>
2026-08-05 21:17:39 +0700

    feat(auth-server/cors): configure cors.

--- apps/auth-server/src/index.ts
+++ apps/auth-server/src/index.ts
@@ -1,8 +1,20 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 import { auth } from "@/lib/auth";
 
 export const app = new Hono()
+  .use(
+    "*",
+    cors({
+      origin: "http://localhost:5173",
+      allowHeaders: ["Content-Type", "Authorization"],
+      allowMethods: ["POST", "GET", "OPTIONS"],
+      exposeHeaders: ["Content-Length"],
+      maxAge: 600,
+      credentials: true,
+    }),
+  )
   .get("/", (c) => {
     return c.text("Hello from Auth Server!");
   })
