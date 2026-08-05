1ee47c8bbfb3eefb572d9dbb0ec4662dcf3ad4f7
Ryan Luong <lumintriet@gmail.com>
2026-08-05 20:55:44 +0700

    feat(auth-server): boilerplate code.

---
+++ apps/auth-server/src/serve.ts
@@ -0,0 +1,27 @@
+import { serve } from "@hono/node-server";
+
+import { app } from "@/index";
+
+const server = serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Auth Server is running on http://localhost:${info.port}`);
+  },
+);
+
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
+});
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+});
