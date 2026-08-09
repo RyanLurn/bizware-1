0392159b6d3776278df67efbb60d7a30a62e2a90
Ryan Luong <lumintriet@gmail.com>
2026-08-09 19:25:49 +0700

    feat(user-web/ui): define root route.

---
+++ apps/user-web/src/routes/__root.tsx
@@ -0,0 +1,43 @@
+import type { ReactNode } from "react";
+
+import { Toaster } from "@repo/ui/components/toast";
+import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+
+import styles from "@/globals.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        content: "width=device-width, initial-scale=1",
+        name: "viewport",
+      },
+      {
+        title: "Bizware",
+      },
+    ],
+    links: [
+      { rel: "stylesheet", href: styles },
+      { type: "image/svg+xml", href: "/favicon.svg", rel: "icon" },
+    ],
+  }),
+  shellComponent: AppShell,
+});
+
+function AppShell({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <div className="h-screen">{children}</div>
+        <Toaster />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
