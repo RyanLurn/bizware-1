52625e90a42d43801cb407590a712b6af7790f8f
Ryan Luong <lumintriet@gmail.com>
2026-08-10 11:03:53 +0700

    feat(auth-client/types): infer more types.

--- libs/auth-client/src/types.ts
+++ libs/auth-client/src/types.ts
@@ -1,3 +1,14 @@
+import type { BetterFetchError } from "better-auth/react";
+
 import type { createAuthClient } from "@/create";
 
 export type AuthClient = ReturnType<typeof createAuthClient>;
+
+export type AuthClientErrorCode = keyof AuthClient["$ERROR_CODES"];
+
+export interface AuthClientError extends Pick<
+  BetterFetchError,
+  "message" | "status" | "statusText"
+> {
+  code: AuthClientErrorCode;
+}
