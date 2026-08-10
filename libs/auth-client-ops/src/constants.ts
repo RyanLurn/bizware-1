d9a467b06536400d7b2ef3767101fcebf25ad4b6
Ryan Luong <lumintriet@gmail.com>
2026-08-10 19:30:40 +0700

    feat(auth-client-ops): define PasswordTooLongError class.

--- libs/auth-client-ops/src/constants.ts
+++ libs/auth-client-ops/src/constants.ts
@@ -1,3 +1,8 @@
-import { MIN_PASSWORD_LENGTH } from "@repo/auth-core/constants";
+import {
+  MAX_PASSWORD_LENGTH,
+  MIN_PASSWORD_LENGTH,
+} from "@repo/auth-core/constants";
 
 export const PASSWORD_TOO_SHORT_ERROR_MESSAGE = `Password must have at least ${MIN_PASSWORD_LENGTH} characters.`;
+
+export const PASSWORD_TOO_LONG_ERROR_MESSAGE = `Password can't have more than ${MAX_PASSWORD_LENGTH} characters.`;
--- libs/auth-client-ops/src/errors.ts
+++ libs/auth-client-ops/src/errors.ts
@@ -18,3 +18,18 @@ export class PasswordTooShortError extends BaseError<
     });
   }
 }
+
+export class PasswordTooLongError extends BaseError<
+  "PASSWORD_TOO_LONG_ERROR",
+  AuthClientError<"PASSWORD_TOO_LONG">
+> {
+  readonly name = "PasswordTooLongError";
+  readonly code = "PASSWORD_TOO_LONG_ERROR";
+
+  constructor({ cause }: { cause: AuthClientError<"PASSWORD_TOO_LONG"> }) {
+    super({
+      message: PASSWORD_TOO_SHORT_ERROR_MESSAGE,
+      cause,
+    });
+  }
+}
