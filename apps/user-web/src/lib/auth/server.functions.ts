339565474daaaeb6cf44773d190ea9b88da977b0
Ryan Luong <lumintriet@gmail.com>
2026-08-11 19:18:31 +0700

    feat(user-web/auth): create getAuthBaseRouteServerFn.

--- apps/user-web/src/lib/auth/server.functions.ts
+++ apps/user-web/src/lib/auth/server.functions.ts
@@ -1,6 +1,16 @@
 import { createServerFn } from "@tanstack/react-start";
 
-import { authenticationMiddleware } from "@/lib/auth/middlewares.server";
+import {
+  authenticationMiddleware,
+  authEnvProviderMiddleware,
+} from "@/lib/auth/middlewares.server";
+
+export const getAuthBaseRouteServerFn = createServerFn()
+  .middleware([authEnvProviderMiddleware])
+  .handler(({ context }) => {
+    const { baseURL, basePath } = context;
+    return { baseURL, basePath };
+  });
 
 export const getUserServerFn = createServerFn()
   .middleware([authenticationMiddleware])
