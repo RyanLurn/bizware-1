ea87f3053971e4a90b0ba07343113313344e2a31
Ryan Luong <lumintriet@gmail.com>
2026-08-11 07:18:43 +0700

    feat(auth-server/types): infer AppAuthServer type.

--- libs/auth-server/src/types.ts
+++ libs/auth-server/src/types.ts
@@ -1,6 +1,8 @@
 import type { createAuthServer } from "@/create";
+import type { createAppAuthServer } from "@/create";
 
 export type AuthServer = ReturnType<typeof createAuthServer>;
+export type AppAuthServer = ReturnType<typeof createAppAuthServer>;
 
 export type AuthSession = AuthServer["$Infer"]["Session"]["session"];
 export type AuthUser = AuthServer["$Infer"]["Session"]["user"];
