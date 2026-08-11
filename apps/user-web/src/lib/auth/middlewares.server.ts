71e17e38e1700b48e45f45c2c9f5ddaf23b7e526
Ryan Luong <lumintriet@gmail.com>
2026-08-11 17:40:37 +0700

    feat(user-web/auth): create authEnvProviderMiddleware.

---
+++ apps/user-web/src/lib/auth/middlewares.server.ts
@@ -0,0 +1,22 @@
+import { AuthServerEnvSchema } from "@repo/auth-server/env";
+import { redirect } from "@tanstack/react-router";
+import { createMiddleware } from "@tanstack/react-start";
+import { prettifyError } from "zod";
+
+export const authEnvProviderMiddleware = createMiddleware().server(
+  ({ next }) => {
+    const parseEnvResult = AuthServerEnvSchema.safeParse(process.env);
+    if (!parseEnvResult.success) {
+      console.error(prettifyError(parseEnvResult.error));
+      throw redirect({ to: "/500" });
+    }
+    const { AUTH_BASE_URL, AUTH_BASE_PATH, AUTH_SECRETS } = parseEnvResult.data;
+    return next({
+      context: {
+        baseURL: AUTH_BASE_URL,
+        basePath: AUTH_BASE_PATH,
+        secrets: AUTH_SECRETS,
+      },
+    });
+  },
+);
