3e2efdbddfc7ad102cacb84976ac4a4181dd6966
Ryan Luong <lumintriet@gmail.com>
2026-08-12 10:17:57 +0700

    feat(auth-tanstack-start/middlewares): create authProviderMiddleware.

---
+++ auth-libs/tanstack-start/src/middlewares.ts
@@ -0,0 +1,48 @@
+import type { AuthBasePath, AuthBaseUrl } from "@repo/auth-core/schemas";
+import type { ValidateRedirectOptions } from "@tanstack/react-router";
+
+import { createAuthServerOptions } from "@repo/auth-server/create";
+import { AuthServerEnvSchema } from "@repo/auth-server/env";
+import { dbProviderMiddleware } from "@repo/db-tanstack-start";
+import { redirect } from "@tanstack/react-router";
+import { createMiddleware } from "@tanstack/react-start";
+import { betterAuth } from "better-auth/minimal";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export function authProviderMiddleware({
+  baseURL,
+  basePath,
+  onErrorRedirectOptions,
+}: {
+  baseURL: AuthBaseUrl;
+  basePath: AuthBasePath;
+  onErrorRedirectOptions: ValidateRedirectOptions;
+}) {
+  return createMiddleware()
+    .middleware([dbProviderMiddleware(onErrorRedirectOptions)])
+    .server(({ next, context }) => {
+      // Get secrets string from env
+      const parseEnvResult = AuthServerEnvSchema.safeParse(process.env);
+      if (!parseEnvResult.success) {
+        console.error(parseEnvResult.error.issues);
+        throw redirect(onErrorRedirectOptions);
+      }
+
+      // Create shared auth server options
+      const sharedOptions = createAuthServerOptions({
+        db: context.db,
+        baseURL,
+        basePath,
+        secrets: parseEnvResult.data.AUTH_SECRETS,
+      });
+
+      // Create auth server instance specific to TanStack Start
+      const auth = betterAuth({
+        ...sharedOptions,
+        plugins: [tanstackStartCookies()],
+      });
+
+      // Pass the instance to the next middleware/function in the chain
+      return next({ context: { auth } });
+    });
+}
